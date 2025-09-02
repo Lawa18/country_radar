54b7d0a5ea1aba0b0b015040065ada56158702ad
Lawa18 <95441357+Lawa18@users.noreply.github.com>
2025-09-01 20:56:49 -0400

    Update eurostat_provider.py

--- app/providers/eurostat_provider.py
+++ app/providers/eurostat_provider.py
@@ -1,1 +1,7 @@
-# functions to fetch Eurostat data
+from typing import Dict
+
+def eurostat_debt_to_gdp_annual(iso2: str) -> Dict[str, float]:
+    """
+    TODO: Implement real Eurostat fetch. Stubbed to {} so service can boot.
+    """
+    return {}
